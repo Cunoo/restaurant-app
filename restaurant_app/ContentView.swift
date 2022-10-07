7c498b691961fec48062077b54a3cccc6451b4ce
Matus Cuninka <cuninka.matus@gmail.com>
2022-10-07 20:44:58 +0200

    Initial Commit

---
+++ restaurant_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  restaurant_app
+//
+//  Created by Matus Cuninka on 07/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ restaurant_app/restaurant_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  restaurant_appApp.swift
+//  restaurant_app
+//
+//  Created by Matus Cuninka on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct restaurant_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
